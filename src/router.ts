9fe146cd4d0222e06bc263625151dfd6bba39251
Septianata Rizky Pratama <ian.rizkypratama@gmail.com>
2026-05-08 20:48:08 +0700

    feat: enhance router configuration with preload, stale time, and scroll restoration settings

--- src/router.ts
+++ src/router.ts
@@ -2,7 +2,12 @@ import { createRouter } from '@tanstack/react-router'
 
 import { routeTree } from './routeTree.gen'
 
-export const router = createRouter({ routeTree })
+export const router = createRouter({
+  routeTree,
+  defaultPreload: 'intent',
+  defaultStaleTime: 5000,
+  scrollRestoration: true,
+})
 
 declare module '@tanstack/react-router' {
   interface Register {
